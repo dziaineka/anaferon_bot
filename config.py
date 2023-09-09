6e4784e123c9610ea6ac6c14d308696175cd0c46
Andrei Dziaineka <dziaineka@gmail.com>
2023-09-09 10:10:52 +0200

    update python and libs

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
-API_TOKEN = 'PUT_TOKEN_HERE'
-URL_BASE = 'https://api.telegram.org/file/bot' + API_TOKEN + '/'
+from os import getenv
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# Create .env file path.
+dotenv_path = join(dirname(__file__), ".env")
+
+# Load file from the path.
+load_dotenv(dotenv_path)
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+URL_BASE = 'https://api.telegram.org/file/bot' + BOT_TOKEN + '/'
--- main.py
+++ main.py
@@ -1,73 +1,71 @@
 import asyncio
 import logging
 
-from aiogram import Bot, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import exceptions, executor
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
 from aiogram.utils.markdown import text
 
 import config
 from medicines import Medicines
 
 loop = asyncio.get_event_loop()
-bot = Bot(token=config.API_TOKEN, loop=loop)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
 medicines = Medicines()
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message(CommandStart())
 async def cmd_start(message: types.Message):
     """
     Conversation's entry point
     """
-    logging.info('Старт работы бота у пользователя ' +
-                 str(message.from_user.id))
+    logging.info(f"Старт работы бота у пользователя {get_some_name(message)}")
 
-    line1 = 'Привет, этот бот ищет присылаемые ему названия лекарств ' +\
-            'в списке "Расстрельный список препаратов" сайта encyclopatia.ru.'
+    line1 = (
+        "Привет, этот бот ищет присылаемые ему названия лекарств "
+        'в списке "Расстрельный список препаратов" сайта encyclopatia.ru.'
+    )
 
     instructions = text(line1)
 
-    await bot.send_message(message.chat.id,
-                           instructions)
+    await bot.send_message(message.chat.id, instructions)
 
 
-@dp.message_handler()
+@dp.message()
 async def process_text(message: types.Message):
+    if not message.text:
+        return
+
     medicine = message.text.strip()
-
-    logging.info('Пользователь ' + str(message.from_user.id) +
-                 ' cпросил ' + medicine + '.')
-
+    logging.info(f"Пользователь {get_some_name(message)} cпросил {medicine}.")
     descriptions = medicines.get_descriptions(medicine)
 
     for descr in descriptions:
         await bot.send_message(message.chat.id, descr)
 
 
+@dp.startup()
 async def startup(dispatcher: Dispatcher):
-    logging.info('Старт бота.')
+    logging.info("Старт бота.")
     await medicines.load_medicine_list()
 
-async def shutdown(dispatcher: Dispatcher):
-    logging.info('Убиваем бота.')
 
-    await dispatcher.storage.close()
-    await dispatcher.storage.wait_closed()
+def get_some_name(message: types.Message) -> str:
+    if message.from_user:
+        return f"{message.from_user.id} ({message.from_user.username})"
+    else:
+        return f"{message.chat.id} ({message.chat.title})"
 
 
-def main():
-    executor.start_polling(dp,
-                           loop=loop,
-                           skip_updates=True,
-                           on_startup=startup,
-                           on_shutdown=shutdown)
+async def main():
+    await dp.start_polling(bot)
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+    asyncio.run(main())
